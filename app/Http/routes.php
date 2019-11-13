2682f5acb8e696fa567be552dd7e2e0fa5b5f6f0
solembum-php <solembum@sole.com>
2019-11-13 20:32:13 +0200

    add views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,19 +1,28 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function(){
-    echo 'hello';
+//all tasks
+Route::get('/tasks', function() {
+    return view('tasks');
+});
+//save task
+Route::post('/tasks', function() {
+    echo 'save task will be here';
+});
+//delete task
+Route::delete('/tasks/{task}', function() {
+    echo 'delete task will be here';
 });
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+	<title>May Laravel First</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+	<div class="container">
+	    <nav class="navbar navbar-default">
+		<!-- Содержимое Navbar -->
+	    </nav>
+	</div>
+	@yield('content')
+    </body>
+</html>
