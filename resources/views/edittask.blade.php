7dfd7e81cf371a1e78d6321efe8b086b33cd6007
solembum-php <solembum@i.ua>
2019-11-17 14:17:16 +0200

    create edittask.blade.php

---
+++ resources/views/edittask.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('content')
+<!-- Bootstrap шаблон... -->
+<div class="panel-body">
+    <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
+    <!-- Форма новой задачи -->
+    <form action="{{ route('savetask') }}" method="POST" class="form-horizontal">
+        {{ csrf_field() }}
+        <!-- Имя задачи -->
+        <div class="form-group">
+            <label for="task-name" class="col-sm-3 control-label">{{trans('tasks.template.task')}}</label>
+            <div class="col-sm-6">
+                <input type="text" name="name" id="task-name" class="form-control" value="task name must be here">
+            </div>
+        </div
+        <!-- Кнопка добавления задачи -->
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" class="btn btn-default">
+                    <i class="fa fa-plus"></i>{{trans('tasks.template.save_task')}}
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
